bd660c06a3770609d21cd5904135af0d336eb189
Tanmoy Dewanjee <tanmoy.dewanjee.16+github.com>
2022-10-31 22:00:35 +0600

    Introduced lombok 1.18.24
    Introduced lombok to reduce boilerplate code throughout the whole project.

--- src/main/java/com/harshchourasiya/countryapi/Model/Country.java
+++ src/main/java/com/harshchourasiya/countryapi/Model/Country.java
@@ -1,72 +1,28 @@
 package com.harshchourasiya.countryapi.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Country {
 
     @Id
     private String name;
+
     private String code;
+
     private String capital;
+
     private String region;
+
     private String flag;
-
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCode() {
-        return this.code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getCapital() {
-        return this.capital;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
-
-    public String getRegion() {
-        return this.region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getFlag() {
-        return this.flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " name='" + getName() + "'" +
-            ", code='" + getCode() + "'" +
-            ", capital='" + getCapital() + "'" +
-            ", region='" + getRegion() + "'" +
-            ", flag='" + getFlag() + "'" +
-            "}";
-    }
-
-
-
 
 }
 
